74b33adc86c5eba570a45bf8424a6811f062f99a
RHODA <RHODA@LAPTOP-3BUAFALF>
2020-11-02 20:27:05 +0100

    Add project files.

---
+++ EXAM_GT_CSHARP_CLASS_QUESTION5/Dtos/ReturnResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASSESSMENT5WORK.Dtos
+{
+    public class ReturnResponse
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public object ObjectValue { get; set; }
+    }
+}
